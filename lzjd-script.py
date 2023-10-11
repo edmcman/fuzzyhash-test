eebb004235dbb0af00a3ad7150a159b62063a1fa
Edward J. Schwartz <eschwartz@cert.org>
2023-10-11 09:55:09 -0400

    Sort results

--- lzjd-script.py
+++ lzjd-script.py
@@ -18,6 +18,11 @@ funs2 = {fun['fn_addr']: digest(fun['exact_bytes']) for fun in j2['analysis']}
 #pp.pprint(funs)
 
 from itertools import product
-for (fun1_addr, fun1_hash), (fun2_addr, fun2_hash) in product(funs1.items(), funs2.items()):
-    similarity = sim(fun1_hash, fun2_hash)
-    print("%s,%s,%s" % (fun1_addr, fun2_addr, similarity))
+#for (fun1_addr, fun1_hash), (fun2_addr, fun2_hash) in product(funs1.items(), funs2.items()):
+#    similarity = sim(fun1_hash, fun2_hash)
+#    print("%s,%s,%s" % (fun1_addr, fun2_addr, similarity))
+
+sims = [(fun1_addr, fun2_addr, sim(fun1_hash, fun2_hash)) for (fun1_addr, fun1_hash), (fun2_addr, fun2_hash) in product(funs1.items(), funs2.items())]
+sims.sort(key=lambda t: t[2])
+for f1, f2, sim in sims:
+    print("%s,%s,%s" % (f1, f2, sim)) 
